72469eb3a21a1ea13c715368a8e3ceddc5643efe
PaulMagos <paulmagos@icloud.com>
2024-05-12 12:40:41 +0200

    Update to endpoints

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from tweets import get_tweets as get_tweets_func
-from words import get_word_frequency, get_langs
+from words import get_word_frequency, get_langs, check_present
 from schemes import TweetsOutSchema
 from datetime import date as date_type
 from datetime import datetime
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-first_day = datetime.strptime('2024-01-01', '%Y-%m-%d').date()
+first_day = datetime.strptime('2024-01-06', '%Y-%m-%d').date()
 
 @app.get('/get_tweets', response_model=TweetsOutSchema)
 async def get_tweets(lang: str = '', type: str = '', group: str='week', from_: date_type=first_day, to_: date_type=date_type.today()) -> TweetsOutSchema:
@@ -27,6 +27,11 @@ async def get_words(source: str='words', lang: str = 'Italian', min_frequency: i
 @app.get('/get_langs_words')
 async def get_langs_words(source: str='words'):
     return await get_langs(source=source)
+
+@app.get('/check_words_presence', response_model=bool)
+async def get_langs_words(source: str='words', lang: str = 'Italian', min_frequency: int=-1, from_: date_type=first_day, to_: date_type=date_type.today(), filter_type: str='tf_idf') -> bool:
+    return await check_present(source=source, from_=from_, to_=to_, lang=lang, min_frequency=min_frequency, filter_type=filter_type)
+
 
 @app.get("/")
 async def root():
--- words.py
+++ words.py
@@ -10,10 +10,6 @@ base_path = os.path.dirname(__file__)
 async def get_word_frequency(source, lang, min_frequency, from_, to_, filter_type) -> WordsOutSchema:
     data = pd.read_json(f'{base_path}/data/{source}.json', orient='records')
         
-    if min_frequency==-1:
-        data = data[data['frequency']>=data['frequency'].quantile(0.98)]
-    else: 
-        data = data[data['frequency']>=min_frequency]
         
     data['created_at'] = data['created_at'].dt.date
     data = data[data['created_at']>=from_]
@@ -28,8 +24,40 @@ async def get_word_frequency(source, lang, min_frequency, from_, to_, filter_typ
     if from_ != to_:
         data = data.groupby(['word', 'lang']).sum(numeric_only=True).reset_index()
         
+    if min_frequency==-1:
+        data = data[data['frequency']>=data['frequency'].quantile(0.95)]
+    else: 
+        data = data[data['frequency']>=min_frequency]
     return Response(data.to_json(orient="records"), media_type="application/json")
 
 async def get_langs(source):
     data = pd.read_json(f'{base_path}/data/{source}.json', orient='records')
     return data['lang'].unique().tolist()
+
+async def check_present(source, from_, to_, lang, min_frequency, filter_type):
+    if not os.path.exists(f'{base_path}/data/{source}.json'):
+        return False
+    data = pd.read_json(f'{base_path}/data/{source}.json', orient='records')
+        
+    data['created_at'] = data['created_at'].dt.date
+    data = data[data['created_at']>=from_]
+    data = data[data['created_at']<=to_]
+        
+    data = data[data['lang'] == lang] 
+        
+    if source == 'hashtags':
+        data['word'] = data['hashtag']
+        data.drop(columns='hashtag', inplace=True)
+        
+    if from_ != to_:
+        data = data.groupby(['word', 'lang']).sum(numeric_only=True).reset_index()
+        
+    if min_frequency==-1:
+        data = data[data['frequency']>=data['frequency'].quantile(0.95)]
+    else: 
+        data = data[data['frequency']>=min_frequency]
+        
+    res = data
+    if len(res) > 0:
+        return True
+    return False
